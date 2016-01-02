76fb2f3c971b01a6e0e857a21e9653ab2654964f
slipperyNipple <slipperyNipple@flenstad.com>
2016-01-02 18:19:14 +0100

    test

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 // Database stuff
 $_DB = array();
 $_DB['server']   = '127.0.0.1';
-$_DB['port']     = '3306';
+$_DB['port']     = '3307';
 $_DB['username'] = 'alda_user';
-$_DB['password'] = 'alda_password';
-$_DB['database'] = 'alda_db';
+$_DB['password'] = 'wb';
+$_DB['database'] = 'cloud_status';
