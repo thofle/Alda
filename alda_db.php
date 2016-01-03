97b9a4ae0d8e76799b4fa79d968fac2fd3637f37
slipperyNipple <slipperyNipple@flenstad.com>
2016-01-03 15:50:40 +0100

    Added function to fetch logins from db.

--- alda_db.php
+++ alda_db.php
@@ -21,16 +21,37 @@ class alda
     $query = <<<EOL
 SELECT
 	queue_id
-    ,hostname
-    ,message
-    ,is_sms_sent
+  ,hostname
+  ,message
+  ,is_sms_sent
 	,num_sms_recipients
-    ,is_cleared
-    ,cleared_message
-    ,level
-    ,date_created
-    ,date_cleared
-FROM v_al_dashboard
+  ,is_cleared
+  ,cleared_message
+  ,level
+  ,date_created
+  ,date_cleared
+FROM
+  v_al_dashboard
+LIMIT 100;
+EOL;
+
+    $handle = $this->db_handle->query($query);
+    $result = $handle->fetchAll(PDO::FETCH_NAMED);
+    return $result;
+  }
+
+  function getLastLogins()
+  {
+    $query = <<<EOL
+SELECT
+  host_id
+  ,username
+  ,remote_addr
+  ,login_timestamp
+FROM
+  sd_logins
+ORDER BY
+  login_timestamp DESC
 LIMIT 100;
 EOL;
 
