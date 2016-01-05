c86d5bcc82255844757649aee1b1d800fb8f7149
slipperyNipple <slipperyNipple@flenstad.com>
2016-01-05 20:43:00 +0100

    Fixed typo

--- design/menu.php
+++ design/menu.php
@@ -2,11 +2,11 @@
 <div id="menu">
   <ul>
     <li><a href="index.php?page=alert-dashboard">Alert Dashboard</a></li>
-    <li class="category">Computers</li>
+    <li class="category">Hosts</li>
     <?php
       foreach ($alda->getHosts() as $host)
       {
-        echo '<li class="subitem"><a href="index.php?page=computers?host_id='.$host['host_id'].'">'.$host['hostname'].'</a></li>';
+        echo '<li class="subitem"><a href="index.php?page=hosts?host_id='.$host['host_id'].'">'.$host['hostname'].'</a></li>';
       }
     ?>
   </ul>
